413ed7f6ec48d4a85e9bccf788c0c54d4f298612
Raul Castro <alejcastro@Alejandros-MacBook-Pro.local>
2023-08-17 00:55:48 -0400

    create a new project

---
+++ assets/js/components/app.js
@@ -0,0 +1,11 @@
+function contactButton() {
+    const contactButton = document.querySelector('.contact__button');
+    
+    const formContainer = document.querySelector('.form__container');
+    
+    contactButton.addEventListener('click', function () {
+        formContainer.classList.toggle('active');
+    });
+}
+
+export default contactButton
---
+++ assets/js/main.js
@@ -0,0 +1,47 @@
+import loader from "./components/loader.js";
+import showMenu from "./components/showMenu.js";
+import showCart from "./components/showCart.js";
+import products from "./components/product.js";
+import getProducts from "./helpers/getProducts.js";
+import cart from "./components/cart.js";
+import contactButton from "./components/app.js";
+import dataForm from "./components/dataForm.js";
+import modalMembers from "./components/modalMembers.js";
+import modalThanks from "./components/modalThanks.js";
+import modalCheckout from "./components/modalCheckout.js";
+import modalStock from "./components/modalStock.js";
+
+
+// Ocultar loader
+loader()
+
+// Mostrar Menu
+showMenu()
+
+// Mostrar carrito
+showCart()
+
+// Products
+const { db, printProducts } = products(await getProducts())
+
+// Carrito
+cart(db, printProducts)
+
+// Boton de Contacto
+contactButton()
+
+// dataForm al correo
+dataForm()
+
+// Modal Integrantes
+modalMembers()
+
+// Modal Thanks
+modalThanks()
+
+// Modal Checkout
+modalCheckout()
+
+// Modal Stock
+modalStock()
+
